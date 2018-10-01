2afae7a41f9741b57334ad8477b53e2f7dff9390
Lindsay Braviak <lbraviak@gmail.com>
2018-10-01 22:50:16 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,9 @@
 def starts_with_a_vowel?(word)
-  word[0].match(/[aeiouAEIOU]/)
+  if word[0].match(/[aeiouAEIOU]/) != nil
+    true
+  else
+    false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
